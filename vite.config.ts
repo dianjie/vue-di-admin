236520e444565b89cb029e0770ff5ecada818a19
xiaoyu <dianjie1022@gmail.com>
2022-12-30 16:10:13 +0800

    fix: gh-pages route error

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
+import { loadEnv, type ConfigEnv, type UserConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+const CWD = process.cwd()
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }: ConfigEnv): UserConfig => {
+  const { VITE_BASE_URL } = loadEnv(mode, CWD)
+  return {
+    base: VITE_BASE_URL,
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
-})
+}
