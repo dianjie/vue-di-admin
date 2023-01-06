7e9f55d89fa8b2dcd363e8ba40247b90ccbfaa91
xiaoyu <dianjie1022@gmail.com>
2023-01-06 14:00:34 +0800

    chore: add element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 const CWD = process.cwd()
 
@@ -11,7 +14,16 @@ export default ({ mode }: ConfigEnv): UserConfig => {
   const { VITE_BASE_URL } = loadEnv(mode, CWD)
   return {
     base: VITE_BASE_URL,
-    plugins: [vue(), vueJsx()],
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
