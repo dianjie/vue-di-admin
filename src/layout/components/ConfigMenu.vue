5009b8004a00242fc5a9d5964535568559df4b38
xiaoyu <dianjie1022@gmail.com>
2023-01-13 15:17:24 +0800

    feat: 常用css属性提取到基础文件中

--- src/layout/components/ConfigMenu.vue
+++ src/layout/components/ConfigMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <el-divider>菜单模式</el-divider>
-  <el-radio-group v-model="store.menuMode" style="justify-content: center; display: flex">
+  <el-radio-group v-model="store.menuMode" class="flex justify-center">
     <el-radio-button label="default">默认</el-radio-button>
     <el-radio-button label="overlay">抽屉</el-radio-button>
     <el-radio-button label="horizontal">垂直</el-radio-button>
--- src/layout/components/ConfigSize.vue
+++ src/layout/components/ConfigSize.vue
@@ -1,6 +1,6 @@
 <template>
   <el-divider>全局组件大小</el-divider>
-  <el-radio-group v-model="store.size" style="justify-content: center; display: flex">
+  <el-radio-group v-model="store.size" class="flex justify-center">
     <el-radio-button label="small">小</el-radio-button>
     <el-radio-button label="default">默认</el-radio-button>
     <el-radio-button label="large">大</el-radio-button>
