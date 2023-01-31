2e082b887b31fdbed7a465aa72c894fd809ea3f0
xiaoyu <dianjie1022@gmail.com>
2023-01-31 16:54:57 +0800

    feat: 固定header高度

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -12,34 +12,15 @@ import BaseHeader from './components/BaseHeader.vue'
 import BaseMenu from './components/BaseMenu.vue'
 import BaseContent from './components/BaseContent.vue'
 import BaseConfig from './components/BaseConfig.vue'
-import type { SizeType } from '#/config'
 
 import { useDesign } from '@/hooks/web/useDesign'
-import { computed, inject, unref } from 'vue'
+import { computed } from 'vue'
 
 const { prefixCls } = useDesign('layout-wrapper')
 
-const size = inject<SizeType>('size')
-
 const classCls = computed(() => [
   {
-    [prefixCls]: true,
-    [`${prefixCls}--${unref(size)}`]: true
+    [prefixCls]: true
   }
 ])
 </script>
-
-<style lang="less" scoped>
-@prefix-cls: ~'@{prefix-var}-layout-wrapper';
-.@{prefix-cls} {
-  &--small {
-    --di-header-height: 46px;
-    --di-menu-horizontal-height: 37px;
-  }
-
-  &--large {
-    --di-header-height: 66px;
-    --di-menu-horizontal-height: 52px;
-  }
-}
-</style>
